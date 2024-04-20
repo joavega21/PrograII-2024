1e3f9c3f94eb88c84e4bb314d226a2209627e8ef
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 19:49:26 -0700

    Validacion de edad ingresada por el usuario utilizando While

---
+++ ValidaciondeEdad/ValidaciondeEdad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidaciondeEdad {
+    class Program {
+        static void Main(string[] args) {
+            Boolean continuar = true;
+            while (continuar) {
+                int edad = 0;
+                Console.Write("Dime tu edad: ");
+                edad = int.Parse(Console.ReadLine());
+                if (edad <= 2) {
+                    Console.WriteLine("Eres un bebe");
+                } else if (edad < 12){
+                    Console.WriteLine("Eres un niño");
+                } else if (edad < 18){
+                    Console.WriteLine("Eres un adolescente");
+                } else if (edad <= 65){
+                    Console.WriteLine("Eres un adulto");
+                } else{
+                    Console.WriteLine("Larga vida");
+                }
+                Console.WriteLine("Si desea continuar presione cualquier tecla, si desea salir presione s: ");
+                continuar = !Console.ReadLine().Equals("s");
+            }
+        }
+    }
+}
