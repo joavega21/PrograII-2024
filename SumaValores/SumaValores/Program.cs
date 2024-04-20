d048161bd167cc6e38a781cd031bef20ba6d5319
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 19:54:49 -0700

    Suma de dos valores ingresados por el usuario

---
+++ SumaValores/SumaValores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaValores {
+    class Program {
+        static void Main(string[] args) {
+            int n1 = 0, n2 = 0, res = 0;
+            Console.Write("Ingrese N1: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese N2: ");
+            n2 = int.Parse(Console.ReadLine());
+            res = n1 + n2;
+            Console.Write("El resultado es: " + res);
+            Console.ReadKey();
+        }
+    }
+}
