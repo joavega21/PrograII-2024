2e8ffbd067246946aab174e4d4bed27f506d558f
joavega21 <isnp151323@ugb.edu.sv>
2024-04-19 20:41:20 -0700

    Programa que muestra secuencia de Fibonacci

---
+++ SecuenciaFibonacci/SecuenciaFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecuenciaFibonacci {
+    class Program {
+        static void Main(string[] args) {
+            Boolean continuar = true;
+            while (continuar) {
+                Console.Clear();
+                int a = 1, b = 0, c = 0;
+                do {
+                    Console.WriteLine("{0} + {1} = {2}", a, b, c);
+                    c = a + b;
+                    b = a;
+                    a = c;
+                } while (c < 1000);
+            }
+        }
+    }
+}
